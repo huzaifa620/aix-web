5152326235678b2bdfa680c3dad6267e74198f0b
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2024-12-26 04:02:45 +0500

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -69,10 +69,10 @@ import TeamSection from '#components/Team'
 import ContactUsSection from '#components/ContactUs'
 import Footer from '#components/Footer'
 
-export const metadata = {
-  title: 'Marketing Page',
-  description: 'This is the marketing page of our app.',
-};
+// export const meta: Metadata = {
+//   title: 'AIgnitiveX',
+//   description: 'Where Artificial Intelligence meets human ingenuity',
+// }
 
 const Home: NextPage = () => {
   return (
